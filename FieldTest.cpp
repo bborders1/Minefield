7357563503b6a171527c31640cd41056e4a39092
Brandon <bborders1@mail.csuchico.edu>
2016-09-26 11:50:30 -0700

    Added several tests, discovered revealAdjacent bug

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -21,4 +21,35 @@ TEST(FieldTest, placeMineInBounds)
 	
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+	ASSERT_ANY_THROW( minefield.get(12,12) );
+}
+
+TEST(FieldTest, checkIsSafe)
+{
+	Field minefield;
+
+	minefield.placeMine(4,5);
+	ASSERT_EQ(false, minefield.isSafe(4,5));
+	ASSERT_EQ(true, minefield.isSafe(5,5));
+}
+
+TEST(FieldTest, fieldType)
+{
+	Field minefield;
+
+	minefield.placeMine(4,5);
+	ASSERT_EQ(MINE_HIDDEN, minefield.get(4,5));
+	ASSERT_EQ(EMPTY_HIDDEN, minefield.get(5,5));
+}
+
+TEST(FieldTest, reveal)
+{
+	Field minefield;
+
+	minefield.placeMine(4,5);
+	minefield.revealAdjacent(5,5);
+	ASSERT_EQ(EMPTY_SHOWN, minefield.get(5,5));
+	ASSERT_EQ(EMPTY_SHOWN, minefield.get(5,6));
+	ASSERT_EQ(EMPTY_SHOWN, minefield.get(6,6));
+	ASSERT_EQ(MINE_HIDDEN, minefield.get(4,5));
 }
--- buglog.md
+++ buglog.md
@@ -1,1 +1,4 @@
 # BUG LOG
+
+# revealAdjacent does not reveal the square clicked on, only the adjacent squares. 
+# revealAdjacent somehow turns the square clicked on into a hidden mine?!? How?!? This occured when square clicked was EMPTY_HIDDEN and adjacent to a hidden mine.
